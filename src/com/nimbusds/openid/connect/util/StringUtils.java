5e0947baf7113802afd502d0b5297ec6bb8ef3a4
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2012-10-10 09:08:07 +0300

    Adds isDefined() to StringUtils

--- src/com/nimbusds/openid/connect/util/StringUtils.java
+++ src/com/nimbusds/openid/connect/util/StringUtils.java
@@ -11,6 +11,24 @@ public class StringUtils {
 
 
 	/**
+	 * Returns {@code true} if the specified string is not {@code null} and
+	 * contains non-whitespace characters.
+	 *
+	 * @param s The string to check. May be {@code null}.
+	 *
+	 * @return {@code true} if the string is not {@code null} and contains
+	 *         non-whitespace characters, else {@code false}.
+	 */
+	public static boolean isDefined(final String s) {
+	
+		if (s != null && ! s.trim().isEmpty())
+			return true;
+		else
+			return false;
+	}
+	
+	
+	/**
 	 * Returns {@code true} if the specified string is {@code null} or
 	 * contains whitespace characters only.
 	 *
