5cc9f1cbea0a61c7e144ff29167d0dad7096af91
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2012-10-11 16:05:56 +0300

    Removes redundant import in IDTokenClaimsRequest

--- src/com/nimbusds/openid/connect/messages/IDTokenClaimsRequest.java
+++ src/com/nimbusds/openid/connect/messages/IDTokenClaimsRequest.java
@@ -3,7 +3,6 @@ package com.nimbusds.openid.connect.messages;
 
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
