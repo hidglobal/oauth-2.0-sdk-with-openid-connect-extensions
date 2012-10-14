c27fbd00973b70b343f8a3502c82ff86c0830fb4
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2012-10-14 09:30:41 +0300

    Updates ResolvedAuthorizationRequest

--- src/com/nimbusds/openid/connect/messages/ResolvedAuthorizationRequest.java
+++ src/com/nimbusds/openid/connect/messages/ResolvedAuthorizationRequest.java
@@ -1,44 +1,18 @@
 package com.nimbusds.openid.connect.messages;
 
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
-
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
-
-import com.nimbusds.jose.JOSEObject;
-import com.nimbusds.jose.JWEObject;
-import com.nimbusds.jose.JWSObject;
-
-import com.nimbusds.jwt.EncryptedJWT;
 import com.nimbusds.jwt.JWT;
-import com.nimbusds.jwt.JWTParser;
-import com.nimbusds.jwt.SignedJWT;
-
-import net.minidev.json.JSONObject;
-
-import com.nimbusds.openid.connect.ParseException;
-import com.nimbusds.openid.connect.SerializeException;
 
 import com.nimbusds.openid.connect.claims.ClientID;
-
-import com.nimbusds.openid.connect.http.HTTPRequest;
-
-import com.nimbusds.openid.connect.util.URLUtils;
 
 
 /**
  * Resolved authorisation request.
  *
  * @author Vladimir Dzhuvinov
- * @version $version$ (2012-10-09)
+ * @version $version$ (2012-10-14)
  */
 public class ResolvedAuthorizationRequest {
 
@@ -46,44 +20,44 @@ public class ResolvedAuthorizationRequest {
 	/**
 	 * The response type set (required).
 	 */
-	private final ResponseTypeSet rts = null;
+	private final ResponseTypeSet rts;
 	
 	
 	/**
 	 * The client identifier (required).
 	 */
-	private final ClientID clientID = null;
+	private final ClientID clientID;
 	
 	
 	/**
 	 * The redirection URI where the response will be sent (required). 
 	 */
-	private final URL redirectURI = null;
+	private final URL redirectURI;
 	
 	
 	/**
 	 * The nonce (required for implicit flow, optional for code flow).
 	 */
-	private final Nonce nonce = null;
+	private final Nonce nonce;
 	
 	
 	/**
 	 * The opaque value to maintain state between the request and the 
 	 * callback (recommended).
 	 */
-	private final State state = null;
+	private final State state;
 	
 	
 	/**
 	 * The requested display type (optional).
 	 */
-	private final Display display = null;
+	private final Display display;
 	
 	
 	/**
 	 * The requested prompt (optional).
 	 */
-	private final Prompt prompt = null;
+	private final Prompt prompt;
 	
 	
 	/**
@@ -91,8 +65,211 @@ public class ResolvedAuthorizationRequest {
 	 * authenticated session with the client (optional). Should be present 
 	 * if {@code prompt=none} is sent.
 	 */
-	private final JWT idTokenHint = null;
+	private final JWT idTokenHint;
+	
+	
+	/**
+	 * The resolved ID Token claims request.
+	 */
+	private final IDTokenClaimsRequest idTokenClaimsRequest;
+	
+	
+	/**
+	 * The resolved UserInfo claims request.
+	 */
+	private final UserInfoClaimsRequest userInfoClaimsRequest;
 	
 	
 	
+	/**
+	 * Creates a new resolved authorisation request.
+	 *
+	 * @param rts            The response type set. Corresponds to the 
+	 *                       {@code response_type} parameter. Must not be
+	 *                       {@code null}.
+	 * @param clientID       The client identifier. Corresponds to the
+	 *                       {@code client_id} parameter. Must not be 
+	 *                       {@code null}.
+	 * @param redirectURI    The redirection URI. Corresponds to the
+	 *                       {@code redirect_uri} parameter. Must not be 
+	 *                       {@code null}.
+	 * @param nonce          The nonce. Corresponds to the {@code nonce} 
+	 *                       parameter. May be {@code null} for code flow.
+	 * @param state          The state. Corresponds to the recommended 
+	 *                       {@code state} parameter. {@code null} if not 
+	 *                       specified.
+	 * @param display        The requested display type. Corresponds to the 
+	 *                       optional {@code display} parameter. 
+	 *                       {@code null} if not specified.
+	 * @param prompt         The requested prompt. Corresponds to the 
+	 *                       optional {@code prompt} parameter. {@code null}
+	 *                       if not specified.
+	 * @param idTokenHint    The ID Token hint. Corresponds to the optinal 
+	 *                       {@code id_token_hint} parameter. {@code null}
+	 *                       if not specified.
+	 * @param idTokenClaims  The resolved ID Token claims request. Must not 
+	 *                       be {@code null}.
+	 * @param userInfoClaims The resolved UserInfo claims request.
+	 */
+	public ResolvedAuthorizationRequest(final ResponseTypeSet rts,
+	                                    final ClientID clientID,
+					    final URL redirectURI,
+					    final Nonce nonce,
+					    final State state,
+					    final Display display,
+					    final Prompt prompt,
+					    final JWT idTokenHint,
+					    final IDTokenClaimsRequest idTokenClaims,
+					    final UserInfoClaimsRequest userInfoClaims) {
+					    
+		
+		if (rts == null)
+			throw new IllegalArgumentException("The response type set must not be null");
+		
+		this.rts = rts;
+		
+		if (clientID == null)
+			throw new IllegalArgumentException("The client ID must not be null");
+			
+		this.clientID = clientID;
+		
+		if (redirectURI == null)
+			throw new IllegalArgumentException("The redirect URI must not be null");
+			
+		this.redirectURI = redirectURI;
+		
+		this.nonce = nonce;
+		
+		this.state = state;
+		
+		this.display = display;
+		
+		this.prompt = prompt;
+		
+		this.idTokenHint = idTokenHint;
+		
+		if (idTokenClaims == null)
+			throw new IllegalArgumentException("The resolved ID Token claims request must not be null");
+		
+		this.idTokenClaimsRequest = idTokenClaims;
+		
+		this.userInfoClaimsRequest = userInfoClaims;
+	}
+	
+	
+	/**
+	 * Gets the response type set. Corresponds to the {@code response_type}
+	 * parameter.
+	 *
+	 * @return The response type set.
+	 */
+	public ResponseTypeSet getResponseTypeSet() {
+	
+		return rts;
+	}
+	
+	
+	/**
+	 * Gets the client identifier. Corresponds to the {@code client_id} 
+	 * parameter.
+	 *
+	 * @return The client identifier.
+	 */
+	public ClientID getClientID() {
+	
+		return clientID;
+	}
+	
+	
+	/**
+	 * Gets the redirection URI. Corresponds to the {@code redirection_uri}
+	 * parameter.
+	 *
+	 * @return The redirection URI.
+	 */
+	public URL getRedirectURI() {
+	
+		return redirectURI;
+	}
+	
+	
+	/**
+	 * Gets the nonce. Corresponds to the {@code nonce} parameter.
+	 *
+	 * @return The nonce, {@code null} if not specified.
+	 */
+	public Nonce getNonce() {
+	
+		return nonce;
+	}
+	
+	
+	/**
+	 * Gets the state. Corresponds to the recommended {@code state} 
+	 * parameter.
+	 *
+	 * @return The state, {@code null} if not specified.
+	 */
+	public State getState() {
+	
+		return state;
+	}
+	
+	
+	/**
+	 * Gets the requested display type. Corresponds to the optional
+	 * {@code display} parameter.
+	 *
+	 * @return The requested display type, {@code null} if not specified.
+	 */
+	public Display getDisplay() {
+	
+		return display;
+	}
+	
+	
+	/**
+	 * Gets the requested prompt. Corresponds to the optional {@code prompt}
+	 * parameter.
+	 *
+	 * @return The requested prompt, {@code null} if not specified.
+	 */
+	public Prompt getPrompt() {
+	
+		return prompt;
+	}
+	
+	
+	/**
+	 * Gets the ID Token hint. Corresponds to the {@code id_token_hint}
+	 * parameter.
+	 *
+	 * @return The ID Token hint, {@code null} if not specified.
+	 */
+	public JWT getIDTokenHint() {
+	
+		return idTokenHint;
+	}
+	
+	
+	/**
+	 * Gets the resolved ID Token claims request.
+	 *
+	 * @return The ID Token claims request.
+	 */
+	public IDTokenClaimsRequest getIDTokenClaimsRequest() {
+	
+		return idTokenClaimsRequest;
+	}
+	
+	
+	/**
+	 * Gets the resolved UserInfo claims request.
+	 *
+	 * @return The UserInfo claims request.
+	 */
+	public UserInfoClaimsRequest getUserInfoClaimsRequest() {
+	
+		return userInfoClaimsRequest;
+	}
 }
