15997f1ed49cacdf635455e680d234fb8af7c7d1
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2012-11-05 11:20:25 +0200

    Adds RP ApplicationType enumeration

---
+++ src/com/nimbusds/openid/connect/sdk/relyingparty/ApplicationType.java
@@ -0,0 +1,23 @@
+package com.nimbusds.openid.connect.sdk.relyingparty;
+
+
+/**
+ * Enumeration of OpenID Connect client application types.
+ *
+ * @author Vladimir Dzhuvinov
+ * @version $version$ (2012-11-05)
+ */
+public enum ApplicationType {
+
+
+	/**
+	 * Native application.
+	 */
+	NATIVE,
+
+
+	/**
+	 * Web application.
+	 */
+	WEB;
+}
---
+++ src/com/nimbusds/openid/connect/sdk/relyingparty/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * OpenID Connect relying party (RP) classes.
+ *
+ * @author Vladimir Dzhuvinov
+ * @version $version$ (2012-11-05)
+ */
+package com.nimbusds.openid.connect.sdk.relyingparty;
