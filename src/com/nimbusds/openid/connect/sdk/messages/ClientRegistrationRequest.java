11509adb745eaf049539d6e7a9f574169ba60dfb
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2012-12-17 18:05:28 +0200

    Adds ClientRegistrationRequest.parse() method stub

--- src/com/nimbusds/openid/connect/sdk/messages/ClientRegistrationRequest.java
+++ src/com/nimbusds/openid/connect/sdk/messages/ClientRegistrationRequest.java
@@ -26,6 +26,7 @@ import com.nimbusds.openid.connect.sdk.relyingparty.ApplicationType;
 import com.nimbusds.openid.connect.sdk.http.CommonContentTypes;
 import com.nimbusds.openid.connect.sdk.http.HTTPRequest;
 
+import com.nimbusds.openid.connect.sdk.util.StringUtils;
 import com.nimbusds.openid.connect.sdk.util.URLUtils;
 
 
@@ -1103,5 +1104,61 @@ public abstract class ClientRegistrationRequest implements Request {
 		httpRequest.setContentType(CommonContentTypes.APPLICATION_URLENCODED);
 
 		return httpRequest;
+	}
+
+
+	/**
+	 * Parses a client registration request from the specified HTTP POST
+	 * request.
+	 *
+	 * <p>Example HTTP request (GET):
+	 *
+	 * <pre>
+	 * POST /connect/register HTTP/1.1
+	 * Accept: application/x-www-form-urlencoded
+	 * Host: server.example.com
+	 * Authorization: Bearer eyJhbGciOiJSUzI1NiJ9.eyJ ... fQ.8Gj_-sj ... _X
+	 * 
+	 * type=client_associate
+	 * &application_type=web
+	 * &redirect_uris=https://client.example.org/callback
+	 *     %20https://client.example.org/callback2
+	 * &application_name=My%20Example%20
+	 * &application_name%23ja-Hani-JP=
+	 * &logo_url=https://client.example.org/logo.png
+	 * &user_id_type=pairwise
+	 * &sector_identifier_url=
+	 *     https://othercompany.com/file_of_redirect_uris_for_our_sites.js
+	 * &token_endpoint_auth_type=client_secret_basic
+	 * &jwk_url=https://client.example.org/my_rsa_public_key.jwk
+	 * &userinfo_encrypted_response_alg=RSA1_5
+	 * &userinfo_encrypted_response_enc=A128CBC+HS256
+	 * </pre>
+	 *
+	 * @param httpRequest The HTTP request. Must not be {@code null}.
+	 *
+	 * @return The parsed client registration request.
+	 *
+	 * @throws ParseException If the HTTP request couldn't be parsed to a 
+	 *                        valid client registration request.
+	 */
+	public static ClientRegistrationRequest parse(final HTTPRequest httpRequest)
+		throws ParseException {
+	
+		Map <String,String> params = URLUtils.parseParameters(httpRequest.getQuery());
+		
+		String v = null;
+		
+		// Mandatory params
+
+		v = params.get("type");
+		
+		if (StringUtils.isUndefined(v))
+			throw new ParseException("Missing \"type\" parameter",
+				                 ErrorCode.INVALID_REQUEST);
+			
+		URL redirectURI = null;
+
+		return null;
 	}
 }
