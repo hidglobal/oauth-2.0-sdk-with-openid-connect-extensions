f9d46e8e368f3cb4970d6388265be0d0ead58071
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2012-12-19 11:56:34 +0200

    Adds base abstract ClientRegistrationResponse class

---
+++ src/com/nimbusds/openid/connect/sdk/messages/ClientRegistrationResponse.java
@@ -0,0 +1,51 @@
+package com.nimbusds.openid.connect.sdk.messages;
+
+
+import com.nimbusds.openid.connect.sdk.claims.ClientID;
+
+
+/**
+ * The base abstract class for client registration responses.
+ *
+ * <p>Related specifications:
+ *
+ * <ul>
+ *     <li>OpenID Connect Dynamic Client Registration 1.0, section 2.2.
+ * </ul>
+ *
+ * @author Vladimir Dzhuvinov
+ * @version $version$ (2012-12-19)
+ */
+public abstract class ClientRegistrationResponse implements SuccessResponse {
+
+
+	/**
+	 * The client ID.
+	 */
+	private ClientID clientID;
+
+
+	/**
+	 * Creates a new client registration response.
+	 *
+	 * @param clientID The client ID. Must not be {@code null}.
+	 */
+	protected ClientRegistrationResponse(final ClientID clientID) {
+
+		if (clientID == null)
+			throw new IllegalArgumentException("The client ID must not be null");
+
+		this.clientID = clientID;
+	}
+
+
+	/**
+	 * Gets the client ID.
+	 *
+	 * @return The client ID.
+	 */
+	public ClientID getClientID() {
+
+		return clientID;
+	}
+}
