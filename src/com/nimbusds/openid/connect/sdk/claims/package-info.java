e1afed24b8ff5665e7788a3bc3ea289f5f07e477
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2013-01-11 18:14:26 +0200

    Updates claims package info

--- src/com/nimbusds/openid/connect/sdk/claims/package-info.java
+++ src/com/nimbusds/openid/connect/sdk/claims/package-info.java
@@ -1,11 +1,11 @@
 /**
- * Claim interface and implementations used by OpenID Connect.
+ * Claim interface and implementations of all standard OpenID Connect claims.
  *
  * <p>A claim is a piece of information about an entity that a claims provider 
  * asserts about that entity. It has a case-sensitive name and a value that 
  * serialises to a JSON boolean, string, number, array, object or null.
  *
- * <p>All OpenID-Connect-related claims contained in this package implement
+ * <p>All OpenID-Connect-related claims in this package implement
  * {@link com.nimbusds.openid.connect.sdk.claims.Claim}.
  *
  * @author Vladimir Dzhuvinov
