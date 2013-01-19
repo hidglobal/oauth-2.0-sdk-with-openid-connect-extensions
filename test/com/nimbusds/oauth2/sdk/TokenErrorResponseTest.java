a88f46b7051121ae8d636a0ec429d72793387a68
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2013-01-19 17:54:58 +0200

    Adds token error response JUnit test

---
+++ test/com/nimbusds/oauth2/sdk/TokenErrorResponseTest.java
@@ -0,0 +1,87 @@
+package com.nimbusds.oauth2.sdk;
+
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import java.util.Map;
+import java.util.Set;
+
+import junit.framework.TestCase;
+
+import net.minidev.json.JSONObject;
+
+import com.nimbusds.oauth2.sdk.http.CommonContentTypes;
+import com.nimbusds.oauth2.sdk.http.HTTPResponse;
+
+import com.nimbusds.oauth2.sdk.util.JSONObjectUtils;
+import com.nimbusds.oauth2.sdk.util.URLUtils;
+
+
+/**
+ * Tests token error response serialisation and parsing.
+ *
+ * @author Vladimir Dzhuvinov
+ * @version $version$ (2013-01-19)
+ */
+public class TokenErrorResponseTest extends TestCase {
+	
+	
+	private static URL ERROR_PAGE_URL = null;
+	
+	
+	public void setUp()
+		throws MalformedURLException {
+		
+		ERROR_PAGE_URL = new URL("http://server.example.com/error/123");
+	}
+
+
+	public void testStandardErrors() {
+	
+		Set<OAuth2Error> errors = TokenErrorResponse.getStandardErrors();
+	
+		assertTrue(errors.contains(OAuth2Error.INVALID_REQUEST));
+		assertTrue(errors.contains(OAuth2Error.INVALID_CLIENT));
+		assertTrue(errors.contains(OAuth2Error.INVALID_GRANT));
+		assertTrue(errors.contains(OAuth2Error.UNAUTHORIZED_CLIENT));
+		assertTrue(errors.contains(OAuth2Error.UNSUPPORTED_GRANT_TYPE));
+		assertTrue(errors.contains(OAuth2Error.INVALID_SCOPE));
+		
+		assertEquals(6, errors.size());
+	}
+	
+	
+	public void testSerializeAndParse()
+		throws Exception {
+	
+		OAuth2Error err = new OAuth2Error(OAuth2Error.INVALID_REQUEST.getValue(),
+			                          OAuth2Error.INVALID_REQUEST.getDescription(),
+			                          ERROR_PAGE_URL);
+
+		TokenErrorResponse r = new TokenErrorResponse(err);
+		
+		assertEquals(OAuth2Error.INVALID_REQUEST, r.getError());
+		
+
+		HTTPResponse httpResponse = r.toHTTPResponse();
+		
+		assertEquals(HTTPResponse.SC_BAD_REQUEST, httpResponse.getStatusCode());
+		assertEquals(CommonContentTypes.APPLICATION_JSON, httpResponse.getContentType());
+		assertEquals("no-store", httpResponse.getCacheControl());
+		assertEquals("no-cache", httpResponse.getPragma());
+		
+		
+		JSONObject jsonObject = JSONObjectUtils.parseJSONObject(httpResponse.getContent());	
+
+		assertEquals(OAuth2Error.INVALID_REQUEST.getValue(), (String)jsonObject.get("error"));
+		assertEquals(OAuth2Error.INVALID_REQUEST.getDescription(), (String)jsonObject.get("error_description"));
+		assertEquals(ERROR_PAGE_URL.toString(), (String)jsonObject.get("error_uri"));
+		assertEquals(3, jsonObject.size());
+		
+		
+		r = TokenErrorResponse.parse(httpResponse);
+		
+		assertEquals(OAuth2Error.INVALID_REQUEST, r.getError());
+	}
+}
