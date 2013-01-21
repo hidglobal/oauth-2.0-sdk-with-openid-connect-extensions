b5960bd12a96a68b9d6cde4570c2496aaf6e300e
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2013-01-21 13:45:56 +0200

    Adds ClaimRequirement enum

---
+++ src/com/nimbusds/openid/connect/sdk/claims/ClaimRequirement.java
@@ -0,0 +1,23 @@
+package com.nimbusds.openid.connect.sdk.claims;
+
+
+/**
+ * Enumeration of the claim requirement types.
+ *
+ * @author Vladimir Dzhuvinov
+ * @version $version$ (2013-01-21)
+ */
+public enum ClaimRequirement {
+
+
+	/**
+	 * Essential claim.
+	 */
+	ESSENTIAL,
+
+
+	/**
+	 * Voluntary claim.
+	 */
+	VOLUNTARY
+}
