cf0bb33259a93baeed1101d8fc9192317a8286d7
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2013-01-24 09:58:31 +0200

    Adds OIDC claims package-info

---
+++ src/com/nimbusds/openid/connect/sdk/claims/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Claims and claim sets used in OpenID Connect.
+ *
+ * @author Vladimir Dzhuvinov
+ * @version $version$ ($version-date$)
+ */
+package com.nimbusds.openid.connect.sdk.claims;
