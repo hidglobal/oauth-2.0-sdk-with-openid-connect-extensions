9874d3831768cb9155f3e0538215a87945048698
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2013-01-30 12:12:06 +0200

    Updates OAuth 2.0 SDK JavaDocs

--- src/com/nimbusds/oauth2/sdk/package-info.java
+++ src/com/nimbusds/oauth2/sdk/package-info.java
@@ -1,8 +1,50 @@
 /**
- * Base OAuth 2.0 classes for representing, serialising and parsing client
- * requests and server responses.
+ * Classes for representing, serialising and parsing OAuth 2.0 client requests
+ * and server responses.
+ *
+ * <p>Authorisation endpoint messages:
+ *
+ * <ul>
+ *     <li>{@link com.nimbusds.oauth2.sdk.AuthorizationRequest} The client
+ *         requests the end-user's authorisation to access a protected
+ *         resource.
+ *     <li>{@link com.nimbusds.oauth2.sdk.AuthorizationResponse} The server
+ *         grants the authorisation or returns an error:
+ *         <ul>
+ *             <li>{@link com.nimbusds.oauth2.sdk.AuthorizationSuccessResponse}
+ *                 The server responds with an authorisation grant.
+ *             <li>{@link com.nimbusds.oauth2.sdk.AuthorizationErrorResponse}
+ *                 The server responds with an authorisation error.
+ *         </ul>
+ *     </li>
+ * </ul>
+ *
+ * <p>Token endpoint messages:
+ *
+ * <ul>
+ *     <li>{@link com.nimbusds.oauth2.sdk.TokenRequest} The client 
+ *         authenticates with the server and requests and access token based on
+ *         a previously issued authorisation code or refresh token:
+ *         <ul>
+ *             <li>{@link com.nimbusds.oauth2.sdk.AccessTokenRequest} The 
+ *                 client requests the authorisation code to be exchanged for 
+ *                 an access and refresh token.
+ *             <li>{@link com.nimbusds.oauth2.sdk.RefreshTokenRequest} The
+ *                 client requests a new access token.
+ *         </ul>
+ *     </li>
+ *     <li>{@link com.nimbusds.oauth2.sdk.TokenResponse} The server responds
+ *         with an access token or returns an error:
+ *         <ul>
+ *             <li>{@link com.nimbusds.oauth2.sdk.AccessTokenResponse} The
+ *                 server responds with an access token and optional refresh
+ *                 token.
+ *             <li>{@link com.nimbusds.oauth2.sdk.TokenErrorResponse} The
+ *                 server responds with a token error.
+ *         </ul>
+ * </ul>
  *
  * @author Vladimir Dzhuvinov
- * @version $version$ (2013-01-21)
+ * @version $version$ ($version-date$)
  */
 package com.nimbusds.oauth2.sdk;
