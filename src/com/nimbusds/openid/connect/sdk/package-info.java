55af2139f994f01783bd1822996575fc6d0a3eaa
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2013-01-30 14:09:48 +0200

    Adds OpenID Connect package-info.java

---
+++ src/com/nimbusds/openid/connect/sdk/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Classes for representing, serialising and parsing OpenID Connect client 
+ * requests and server responses.
+ *
+ * @author Vladimir Dzhuvinov
+ * @version $version$ ($version-date$)
+ */
+package com.nimbusds.openid.connect.sdk;
