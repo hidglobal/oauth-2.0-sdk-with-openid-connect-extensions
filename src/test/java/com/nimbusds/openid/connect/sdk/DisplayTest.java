ad53899454ee2b07567de58443c81272fddaf615
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2013-05-28 10:29:42 +0300

    Adds Display JUnit test

---
+++ src/test/java/com/nimbusds/openid/connect/sdk/DisplayTest.java
@@ -0,0 +1,84 @@
+package com.nimbusds.openid.connect.sdk;
+
+
+import junit.framework.TestCase;
+
+import com.nimbusds.oauth2.sdk.ParseException;
+
+
+/**
+ * Tests the Display class.
+ *
+ * @author Vladimir Dzhuvinov
+ */
+public class DisplayTest extends TestCase {
+
+	public void testToString() {
+
+		assertEquals("page", Display.PAGE.toString());
+		assertEquals("popup", Display.POPUP.toString());
+		assertEquals("touch", Display.TOUCH.toString());
+		assertEquals("wap", Display.WAP.toString());
+	}
+
+
+	public void testDefault() {
+
+		assertEquals(Display.PAGE, Display.getDefault());
+	}
+
+
+	public void testParsePage()
+		throws ParseException {
+
+		assertEquals(Display.PAGE, Display.parse("page"));
+	}
+
+
+	public void testParsePopup()
+		throws ParseException {
+
+		assertEquals(Display.POPUP, Display.parse("popup"));
+	}
+
+
+	public void testParseTouch()
+		throws ParseException {
+
+		assertEquals(Display.TOUCH, Display.parse("touch"));
+	}
+
+
+	public void testParseWap()
+		throws ParseException {
+
+		assertEquals(Display.WAP, Display.parse("wap"));
+	}
+
+
+	public void testParseNull()
+		throws ParseException {
+
+		assertEquals(Display.PAGE, Display.parse(null));
+	}
+
+
+	public void testParseEmptyString()
+		throws ParseException {
+
+		assertEquals(Display.PAGE, Display.parse(""));
+	}
+
+
+	public void testParseException() {
+
+		try {
+			Display.parse("some-unsupported-display-type");
+
+			fail("Failed to throw parse exception");
+
+		} catch (ParseException e) {
+			// ok
+		}
+	}
+}
