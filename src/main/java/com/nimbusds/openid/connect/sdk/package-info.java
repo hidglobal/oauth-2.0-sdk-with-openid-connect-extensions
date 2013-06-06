0edc824c747feddc49559165b5d5ccf44b0231e4
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2013-06-06 13:06:05 +0300

    Removes @author from package-info.java in OIDC SDK packages

--- src/main/java/com/nimbusds/openid/connect/sdk/package-info.java
+++ src/main/java/com/nimbusds/openid/connect/sdk/package-info.java
@@ -1,7 +1,5 @@
 /**
  * Classes for representing, serialising and parsing OpenID Connect client 
  * requests and server responses.
- *
- * @author Vladimir Dzhuvinov
  */
 package com.nimbusds.openid.connect.sdk;
--- src/main/java/com/nimbusds/openid/connect/sdk/rp/package-info.java
+++ src/main/java/com/nimbusds/openid/connect/sdk/rp/package-info.java
@@ -1,6 +1,4 @@
 /**
  * OpenID Connect Relying Party (RP) classes.
- *
- * @author Vladimir Dzhuvinov
  */
 package com.nimbusds.openid.connect.sdk.rp;
