48d4fc5b8dca095d06f0e95551b2c5ab4078b4d4
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2013-06-22 14:43:23 +0300

    Adds OIDCClientUpdateRequest

---
+++ src/main/java/com/nimbusds/openid/connect/sdk/rp/OIDCClientUpdateRequest.java
@@ -0,0 +1,131 @@
+package com.nimbusds.openid.connect.sdk.rp;
+
+import com.nimbusds.oauth2.sdk.ParseException;
+import com.nimbusds.oauth2.sdk.auth.Secret;
+import com.nimbusds.oauth2.sdk.client.ClientMetadata;
+import com.nimbusds.oauth2.sdk.client.ClientUpdateRequest;
+import com.nimbusds.oauth2.sdk.http.HTTPRequest;
+import com.nimbusds.oauth2.sdk.id.ClientID;
+import com.nimbusds.oauth2.sdk.token.BearerAccessToken;
+import com.nimbusds.oauth2.sdk.util.JSONObjectUtils;
+import java.net.URL;
+import net.jcip.annotations.Immutable;
+import net.minidev.json.JSONObject;
+import org.apache.commons.lang3.StringUtils;
+
+
+/**
+ * OpenID Connect client registration request. This class is immutable.
+ * 
+ * Note that the update operation is not specified in OpenID Connect Dynamic
+ * Client Registration.
+ * 
+ * <p>Example HTTP request:
+ *
+ * <pre>
+ * PUT /register/s6BhdRkqt3 HTTP/1.1
+ * Accept: application/json
+ * Host: server.example.com
+ * Authorization: Bearer reg-23410913-abewfq.123483
+ *
+ * {
+ *  "client_id"                  :"s6BhdRkqt3",
+ *  "client_secret"              : "cf136dc3c1fc93f31185e5885805d",
+ *  "redirect_uris"              : ["https://client.example.org/callback", "https://client.example.org/alt"],
+ *  "scope"                      : "read write dolphin",
+ *  "grant_types"                : ["authorization_code", "refresh_token"]
+ *  "token_endpoint_auth_method" : "client_secret_basic",
+ *  "jwks_uri"                   : "https://client.example.org/my_public_keys.jwks"
+ *  "client_name"                : "My New Example",
+ *  "client_name#fr"             : "Mon Nouvel Exemple",
+ *  "logo_uri"                   : "https://client.example.org/newlogo.png"
+ *  "logo_uri#fr"                : "https://client.example.org/fr/newlogo.png"
+ * }
+ *
+ * </pre>
+ *
+ * <p>Related specifications:
+ *
+ * <ul>
+ *     <li>OAuth 2.0 Dynamic Client Registration Protocol 
+ *         (draft-ietf-oauth-dyn-reg-12), section 4.3.
+ * </ul>
+ * 
+ * @author Vladimir Dzhuvinov
+ */
+@Immutable
+public final class OIDCClientUpdateRequest extends ClientUpdateRequest {
+	
+	
+	/**
+	 * Creates a new OpenID Connect client update request.
+	 *
+	 * @param uri         The URI of the client update endpoint. May be
+	 *                    {@code null} if the {@link #toHTTPRequest()}
+	 *                    method will not be used.
+	 * @param accessToken The client registration access token. Must not be
+	 *                    {@code null}.
+	 * @param metadata    The client metadata. Must not be {@code null} and 
+	 *                    must specify one or more redirect URIs.
+	 * @param secret      The optional client secret, {@code null} if not
+	 *                    specified.
+	 */
+	public OIDCClientUpdateRequest(final URL uri,
+		                   final ClientID id,
+		                   final BearerAccessToken accessToken,
+				   final OIDCClientMetadata metadata, 
+				   final Secret secret) {
+		
+		super(uri, id, accessToken, metadata, secret);
+	}
+	
+	
+	/**
+	 * Gets the associated OpenID Connect client metadata.
+	 *
+	 * @return The OpenID Connect client metadata.
+	 */
+	public OIDCClientMetadata getOIDCClientMetadata() {
+		
+		return (OIDCClientMetadata)getClientMetadata();
+	}
+	
+	
+	/**
+	 * Parses an OpenID Connect client update request from the specified 
+	 * HTTP PUT request.
+	 *
+	 * @param httpRequest The HTTP request. Must not be {@code null}.
+	 *
+	 * @return The OpenID Connect client update request.
+	 *
+	 * @throws ParseException If the HTTP request couldn't be parsed to an
+	 *                        OpenID Connect client update request.
+	 */
+	public static ClientUpdateRequest parse(final HTTPRequest httpRequest)
+		throws ParseException {
+
+		httpRequest.ensureMethod(HTTPRequest.Method.PUT);
+		
+		String authzHeaderValue = httpRequest.getAuthorization();
+		
+		if (StringUtils.isBlank(authzHeaderValue))
+			throw new ParseException("Missing HTTP Authorization header");
+		
+		BearerAccessToken accessToken = BearerAccessToken.parse(authzHeaderValue);
+		
+		JSONObject jsonObject = httpRequest.getQueryAsJSONObject();
+		
+		ClientID id = new ClientID(JSONObjectUtils.getString(jsonObject, "client_id"));
+
+		OIDCClientMetadata metadata = OIDCClientMetadata.parse(jsonObject);
+		
+		Secret clientSecret = null;
+		
+		if (jsonObject.get("client_secret") != null)
+			clientSecret = new Secret(JSONObjectUtils.getString(jsonObject, "client_secret"));
+			
+		
+		return new OIDCClientUpdateRequest(httpRequest.getURL(), id, accessToken, metadata, clientSecret);
+	}
+}
