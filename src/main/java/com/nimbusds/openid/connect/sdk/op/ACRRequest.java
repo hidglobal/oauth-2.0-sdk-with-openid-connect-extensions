d2e6d7219a1b621518fc8455ca01c1ddf6f40ed4
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2013-07-02 16:38:52 +0300

    Refactors ACR request, adds resolve method (issue #35)

---
+++ src/main/java/com/nimbusds/openid/connect/sdk/op/ACRRequest.java
@@ -0,0 +1,139 @@
+package com.nimbusds.openid.connect.sdk.op;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.jcip.annotations.Immutable;
+
+import com.nimbusds.openid.connect.sdk.ClaimsRequest;
+import com.nimbusds.openid.connect.sdk.OIDCAuthorizationRequest;
+import com.nimbusds.openid.connect.sdk.claims.ACR;
+import com.nimbusds.openid.connect.sdk.claims.ClaimRequirement;
+
+
+/**
+ *  Resolved authentication Context Class Reference (ACR) request.
+ *
+ * @author Vladimir Dzhuvinov
+ */
+@Immutable 
+public final class ACRRequest {
+
+
+	/**
+	 * The essential ACR values.
+	 */
+	private final List<ACR> essentialACRs;
+
+
+	/**
+	 * The voluntary ACR values.
+	 */
+	private final List<ACR> voluntaryACRs;
+
+
+	/**
+	 * Creates a new Authentication Context Class Reference (ACR) request.
+	 *
+	 * @param essentialACRs The requested essential ACR values, by order of
+	 *                      preference, {@code null} if not specified.
+	 * @param voluntaryACRs The requested voluntary ACR values, by order of
+	 *                      preference, {@code null} if not specified.
+	 */
+	public ACRRequest(final List<ACR> essentialACRs, final List<ACR> voluntaryACRs) {
+
+		this.essentialACRs = essentialACRs;
+		this.voluntaryACRs = voluntaryACRs;
+	}
+	
+
+	/**
+	 * Gets the requested essential ACR values.
+	 * 
+	 * @return The essential ACR values, by order of preference, 
+	 *         {@code null} if not specified.
+	 */
+	public List<ACR> getEssentialACRs() {
+		
+		return essentialACRs;
+	}
+	
+	
+	/**
+	 * Gets the requested voluntary ACR values.
+	 * 
+	 * @return The voluntary ACR values, by order of preference, 
+	 *         {@code null} if not specified.
+	 */
+	public List<ACR> getVoluntaryACRs() {
+		
+		return voluntaryACRs;
+	}
+	
+	
+	/**
+	 * Resolves the requested essential and voluntary ACR values from the
+	 * specified OpenID Connect authorisation request.
+	 * 
+	 * @param authzRequest The OpenID Connect authorisation request. Should
+	 *                     be resolved. Must not be {@code null}.
+	 * 
+	 * @return The resolved ACR request.
+	 */
+	public static ACRRequest resolve(final OIDCAuthorizationRequest authzRequest) {
+		
+		List<ACR> essentialACRs = null;
+		List<ACR> voluntaryACRs = null;
+		
+		ClaimsRequest claimsRequest = authzRequest.getClaims();
+		
+		if (claimsRequest != null) {
+			
+			for (ClaimsRequest.Entry claimEntry: claimsRequest.getIDTokenClaims()) {
+				
+				if (! claimEntry.getClaimName().equals("acr"))
+					continue;
+				
+				if (claimEntry.getClaimRequirement().equals(ClaimRequirement.ESSENTIAL)) {
+					
+					essentialACRs = new ArrayList<ACR>();
+					
+					if (claimEntry.getValue() != null)
+						essentialACRs.add(new ACR(claimEntry.getValue()));
+					
+					if (claimEntry.getValues() != null) {
+						
+						for (String v: claimEntry.getValues())
+							essentialACRs.add(new ACR(v));
+					}
+					
+				} else {
+					voluntaryACRs = new ArrayList<ACR>();
+					
+					if (claimEntry.getValue() != null)
+						voluntaryACRs.add(new ACR(claimEntry.getValue()));
+					
+					if (claimEntry.getValues() != null) {
+						
+						for (String v: claimEntry.getValues())
+							voluntaryACRs.add(new ACR(v));
+					}
+				}
+			}
+		}
+		
+		
+		List<ACR> topLevelACRs = authzRequest.getACRValues();
+		
+		if (topLevelACRs != null) {
+			
+			if (voluntaryACRs == null)
+				voluntaryACRs = new ArrayList<ACR>();
+			
+			voluntaryACRs.addAll(topLevelACRs);
+		}
+		
+		return new ACRRequest(essentialACRs, voluntaryACRs);
+	}
+}
---
+++ src/test/java/com/nimbusds/openid/connect/sdk/op/ACRRequestTest.java
@@ -0,0 +1,236 @@
+package com.nimbusds.openid.connect.sdk.op;
+
+
+import com.nimbusds.oauth2.sdk.ResponseType;
+import com.nimbusds.oauth2.sdk.Scope;
+import com.nimbusds.oauth2.sdk.id.ClientID;
+import com.nimbusds.oauth2.sdk.id.State;
+import com.nimbusds.openid.connect.sdk.ClaimsRequest;
+import com.nimbusds.openid.connect.sdk.Display;
+import com.nimbusds.openid.connect.sdk.Nonce;
+import com.nimbusds.openid.connect.sdk.OIDCAuthorizationRequest;
+import com.nimbusds.openid.connect.sdk.Prompt;
+import com.nimbusds.openid.connect.sdk.claims.ACR;
+import com.nimbusds.openid.connect.sdk.claims.ClaimRequirement;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+
+/**
+ * Tests the ACR request class.
+ * 
+ * @author Vladimir Dzhuvinov
+ */
+public class ACRRequestTest {
+	
+	
+	@Test
+	public void testConstructAndGet() {
+		
+		List<ACR> essentialACRs = new ArrayList<ACR>();
+		essentialACRs.add(new ACR("1"));
+		
+		List<ACR> voluntaryACRs = new ArrayList<ACR>();
+		voluntaryACRs.add(new ACR("2"));
+		
+		ACRRequest req = new ACRRequest(essentialACRs, voluntaryACRs);
+		
+		assertEquals(essentialACRs, req.getEssentialACRs());
+		assertEquals(voluntaryACRs, req.getVoluntaryACRs());
+		
+		assertEquals(1, req.getEssentialACRs().size());
+		assertEquals(1, req.getVoluntaryACRs().size());
+	}
+	
+	
+	@Test
+	public void testConstructAndGetNull() {
+		
+		ACRRequest req = new ACRRequest(null, null);
+		
+		assertNull(req.getEssentialACRs());
+		assertNull(req.getVoluntaryACRs());
+	}
+	
+	
+	@Test
+	public void testResolveNone()
+		throws Exception {
+		
+		URL requestURI = new URL("https://c2id.com/authz");
+		ResponseType rt = new ResponseType();
+		rt.add(ResponseType.Value.CODE);
+		Scope scope = Scope.parse("openid profile");
+		ClientID clientID = new ClientID("abc");
+		URL redirectURI = new URL("https://example.com/in");
+		State state = new State();
+		Nonce nonce = new Nonce();
+		
+		
+		OIDCAuthorizationRequest authzRequest = new OIDCAuthorizationRequest(requestURI, 
+			rt, scope, clientID, redirectURI, state, nonce);
+		
+		ACRRequest acrRequest = ACRRequest.resolve(authzRequest);
+		
+		assertNull(acrRequest.getEssentialACRs());
+		assertNull(acrRequest.getVoluntaryACRs());
+	}
+	
+	
+	@Test
+	public void testResolveTopLevelACRRequest()
+		throws Exception {
+		
+		URL requestURI = new URL("https://c2id.com/authz");
+		ResponseType rt = new ResponseType();
+		rt.add(ResponseType.Value.CODE);
+		Scope scope = Scope.parse("openid profile");
+		ClientID clientID = new ClientID("abc");
+		URL redirectURI = new URL("https://example.com/in");
+		State state = new State();
+		Nonce nonce = new Nonce();
+		Display display = Display.POPUP;
+		Prompt prompt = Prompt.parse("login");
+		int maxAge = 3600;
+		List<ACR> acrValues = new ArrayList<ACR>();
+		acrValues.add(new ACR("1"));
+		acrValues.add(new ACR("2"));
+		ClaimsRequest claims = null;
+		
+		OIDCAuthorizationRequest authzRequest = new OIDCAuthorizationRequest(requestURI, 
+			rt, scope, clientID, redirectURI, state, nonce,
+			display, prompt, maxAge, null, null, null, null, acrValues, claims);
+		
+		ACRRequest acrRequest = ACRRequest.resolve(authzRequest);
+		
+		assertNull(acrRequest.getEssentialACRs());
+		
+		List<ACR> voluntaryACRs = acrRequest.getVoluntaryACRs();
+		
+		assertTrue(voluntaryACRs.contains(new ACR("1")));
+		assertTrue(voluntaryACRs.contains(new ACR("2")));
+		
+		assertEquals(2, voluntaryACRs.size());
+	}
+	
+	
+	@Test
+	public void testResolveClaimsLevelEssentialACRRequest()
+		throws Exception {
+		
+		URL requestURI = new URL("https://c2id.com/authz");
+		ResponseType rt = new ResponseType();
+		rt.add(ResponseType.Value.CODE);
+		Scope scope = Scope.parse("openid profile");
+		ClientID clientID = new ClientID("abc");
+		URL redirectURI = new URL("https://example.com/in");
+		State state = new State();
+		Nonce nonce = new Nonce();
+		Display display = Display.POPUP;
+		Prompt prompt = Prompt.parse("login");
+		int maxAge = 3600;
+		List<ACR> acrValues = null;
+		
+		ClaimsRequest claims = new ClaimsRequest();
+		
+		List<String> essentialACRs = new ArrayList<String>();
+		essentialACRs.add("A");
+		essentialACRs.add("B");
+		claims.addIDTokenClaim("acr", ClaimRequirement.ESSENTIAL, null, essentialACRs);
+		
+		OIDCAuthorizationRequest authzRequest = new OIDCAuthorizationRequest(requestURI, 
+			rt, scope, clientID, redirectURI, state, nonce,
+			display, prompt, maxAge, null, null, null, null, acrValues, claims);
+		
+		ACRRequest acrRequest = ACRRequest.resolve(authzRequest);
+		
+		assertTrue(acrRequest.getEssentialACRs().contains(new ACR("A")));
+		assertTrue(acrRequest.getEssentialACRs().contains(new ACR("B")));
+		assertEquals(2, acrRequest.getEssentialACRs().size());
+		
+		assertNull(acrRequest.getVoluntaryACRs());
+	}
+	
+	
+	@Test
+	public void testResolveClaimsLevelVoluntaryACRRequest()
+		throws Exception {
+		
+		URL requestURI = new URL("https://c2id.com/authz");
+		ResponseType rt = new ResponseType();
+		rt.add(ResponseType.Value.CODE);
+		Scope scope = Scope.parse("openid profile");
+		ClientID clientID = new ClientID("abc");
+		URL redirectURI = new URL("https://example.com/in");
+		State state = new State();
+		Nonce nonce = new Nonce();
+		Display display = Display.POPUP;
+		Prompt prompt = Prompt.parse("login");
+		int maxAge = 3600;
+		List<ACR> acrValues = null;
+		
+		ClaimsRequest claims = new ClaimsRequest();
+		
+		List<String> essentialACRs = new ArrayList<String>();
+		essentialACRs.add("A");
+		essentialACRs.add("B");
+		claims.addIDTokenClaim("acr", ClaimRequirement.VOLUNTARY, null, essentialACRs);
+		
+		OIDCAuthorizationRequest authzRequest = new OIDCAuthorizationRequest(requestURI, 
+			rt, scope, clientID, redirectURI, state, nonce,
+			display, prompt, maxAge, null, null, null, null, acrValues, claims);
+		
+		ACRRequest acrRequest = ACRRequest.resolve(authzRequest);
+		
+		assertNull(acrRequest.getEssentialACRs());
+		
+		assertTrue(acrRequest.getVoluntaryACRs().contains(new ACR("A")));
+		assertTrue(acrRequest.getVoluntaryACRs().contains(new ACR("B")));
+		assertEquals(2, acrRequest.getVoluntaryACRs().size());
+	}
+	
+	
+	@Test
+	public void testResolveMixedACRRequest()
+		throws Exception {
+		
+		URL requestURI = new URL("https://c2id.com/authz");
+		ResponseType rt = new ResponseType();
+		rt.add(ResponseType.Value.CODE);
+		Scope scope = Scope.parse("openid profile");
+		ClientID clientID = new ClientID("abc");
+		URL redirectURI = new URL("https://example.com/in");
+		State state = new State();
+		Nonce nonce = new Nonce();
+		Display display = Display.POPUP;
+		Prompt prompt = Prompt.parse("login");
+		int maxAge = 3600;
+		List<ACR> acrValues = new ArrayList<ACR>();
+		acrValues.add(new ACR("1"));
+		acrValues.add(new ACR("2"));
+		
+		ClaimsRequest claims = new ClaimsRequest();
+		
+		List<String> essentialACRs = new ArrayList<String>();
+		essentialACRs.add("A");
+		essentialACRs.add("B");
+		claims.addIDTokenClaim("acr", ClaimRequirement.ESSENTIAL, null, essentialACRs);
+		
+		OIDCAuthorizationRequest authzRequest = new OIDCAuthorizationRequest(requestURI, 
+			rt, scope, clientID, redirectURI, state, nonce,
+			display, prompt, maxAge, null, null, null, null, acrValues, claims);
+		
+		ACRRequest acrRequest = ACRRequest.resolve(authzRequest);
+		
+		assertTrue(acrRequest.getEssentialACRs().contains(new ACR("A")));
+		assertTrue(acrRequest.getEssentialACRs().contains(new ACR("B")));
+		assertEquals(2, acrRequest.getEssentialACRs().size());
+		
+		assertTrue(acrRequest.getVoluntaryACRs().contains(new ACR("1")));
+		assertTrue(acrRequest.getVoluntaryACRs().contains(new ACR("2")));
+		assertEquals(2, acrRequest.getVoluntaryACRs().size());
+	}
+}
