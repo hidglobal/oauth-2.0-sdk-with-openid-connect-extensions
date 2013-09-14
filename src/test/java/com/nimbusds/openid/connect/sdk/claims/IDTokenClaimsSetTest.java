cdc35e808c2bb0d9ce88a60fd94870cc3c6ee9aa
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2013-09-14 13:54:14 +0300

    Extends ID token claims set JUnit tests

--- src/test/java/com/nimbusds/openid/connect/sdk/claims/IDTokenClaimsSetTest.java
+++ src/test/java/com/nimbusds/openid/connect/sdk/claims/IDTokenClaimsSetTest.java
@@ -1,9 +1,19 @@
 package com.nimbusds.openid.connect.sdk.claims;
 
 
-import com.nimbusds.jwt.JWTClaimsSet;
-import com.nimbusds.oauth2.sdk.util.DateUtils;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
 import junit.framework.TestCase;
+
+import com.nimbusds.jwt.JWTClaimsSet;
+
+import com.nimbusds.oauth2.sdk.id.Audience;
+import com.nimbusds.oauth2.sdk.id.Issuer;
+import com.nimbusds.oauth2.sdk.id.Subject;
+import com.nimbusds.oauth2.sdk.util.DateUtils;
+import com.nimbusds.openid.connect.sdk.Nonce;
 
 
 /**
@@ -62,4 +72,77 @@ public class IDTokenClaimsSetTest extends TestCase {
 		assertEquals("MTIzNDU2Nzg5MDEyMzQ1Ng", idTokenClaimsSet.getAccessTokenHash().getValue());
 	}
 
+
+	public void testGettersAndSetters()
+		throws Exception {
+
+		Issuer issuer = new Issuer("iss");
+		Subject subject = new Subject("sub");
+
+		List<Audience> audList = new LinkedList<Audience>();
+		audList.add(new Audience("aud"));
+
+		Date expirationTime = DateUtils.fromSecondsSinceEpoch(100000l);
+		Date issueTime = DateUtils.fromSecondsSinceEpoch(200000l);
+
+		IDTokenClaimsSet idTokenClaimsSet = new IDTokenClaimsSet(issuer, subject, audList, expirationTime, issueTime);
+
+		Date authenticationTime = DateUtils.fromSecondsSinceEpoch(300000l);
+		idTokenClaimsSet.setAuthenticationTime(authenticationTime);
+
+		Nonce nonce = new Nonce();
+		idTokenClaimsSet.setNonce(nonce);
+
+		AccessTokenHash accessTokenHash = new AccessTokenHash("123");
+		idTokenClaimsSet.setAccessTokenHash(accessTokenHash);
+
+		CodeHash codeHash = new CodeHash("456");
+		idTokenClaimsSet.setCodeHash(codeHash);
+
+		ACR acr = new ACR("1");
+		idTokenClaimsSet.setACR(acr);
+
+		List<AMR> amrList = new LinkedList<AMR>();
+		amrList.add(new AMR("A"));
+		idTokenClaimsSet.setAMR(amrList);
+
+		AuthorizedParty authorizedParty = new AuthorizedParty("azp");
+		idTokenClaimsSet.setAuthorizedParty(authorizedParty);
+
+		// Mandatory claims
+		assertEquals("iss", idTokenClaimsSet.getIssuer().getValue());
+		assertEquals("sub", idTokenClaimsSet.getSubject().getValue());
+		assertEquals("aud", idTokenClaimsSet.getAudience().get(0).getValue());
+		assertEquals(100000l, idTokenClaimsSet.getExpirationTime().getTime() / 1000);
+		assertEquals(200000l, idTokenClaimsSet.getIssueTime().getTime() / 1000);
+
+		// Optional claims
+		assertEquals(300000l, idTokenClaimsSet.getAuthenticationTime().getTime() / 1000);
+		assertEquals(nonce.getValue(), idTokenClaimsSet.getNonce().getValue());
+		assertEquals(accessTokenHash.getValue(), idTokenClaimsSet.getAccessTokenHash().getValue());
+		assertEquals(codeHash.getValue(), idTokenClaimsSet.getCodeHash().getValue());
+		assertEquals(acr.getValue(), idTokenClaimsSet.getACR().getValue());
+		assertEquals("A", idTokenClaimsSet.getAMR().get(0).getValue());
+		assertEquals(authorizedParty.getValue(), idTokenClaimsSet.getAuthorizedParty().getValue());
+
+		String json = idTokenClaimsSet.toJSONObject().toJSONString();
+
+		idTokenClaimsSet = IDTokenClaimsSet.parse(json);
+
+		// Mandatory claims
+		assertEquals("iss", idTokenClaimsSet.getIssuer().getValue());
+		assertEquals("sub", idTokenClaimsSet.getSubject().getValue());
+		assertEquals("aud", idTokenClaimsSet.getAudience().get(0).getValue());
+		assertEquals(100000l, idTokenClaimsSet.getExpirationTime().getTime() / 1000);
+		assertEquals(200000l, idTokenClaimsSet.getIssueTime().getTime() / 1000);
+
+		// Optional claims
+		assertEquals(300000l, idTokenClaimsSet.getAuthenticationTime().getTime() / 1000);
+		assertEquals(nonce.getValue(), idTokenClaimsSet.getNonce().getValue());
+		assertEquals(accessTokenHash.getValue(), idTokenClaimsSet.getAccessTokenHash().getValue());
+		assertEquals(codeHash.getValue(), idTokenClaimsSet.getCodeHash().getValue());
+		assertEquals(acr.getValue(), idTokenClaimsSet.getACR().getValue());
+		assertEquals("A", idTokenClaimsSet.getAMR().get(0).getValue());
+		assertEquals(authorizedParty.getValue(), idTokenClaimsSet.getAuthorizedParty().getValue());
+	}
 }
