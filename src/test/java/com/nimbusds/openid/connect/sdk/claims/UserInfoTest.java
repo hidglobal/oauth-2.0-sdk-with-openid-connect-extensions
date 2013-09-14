cc34c49e85973724ff21cd9d34e0bb62264a1ad9
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2013-09-14 10:44:02 +0300

    Extends UserInfo JUnit test

--- src/test/java/com/nimbusds/openid/connect/sdk/claims/UserInfoTest.java
+++ src/test/java/com/nimbusds/openid/connect/sdk/claims/UserInfoTest.java
@@ -48,4 +48,55 @@ public class UserInfoTest extends TestCase {
 		assertEquals("janedoe@example.com", userInfo.getEmail().getAddress());
 		assertEquals("http://example.com/janedoe/me.jpg", userInfo.getPicture().toString());
 	}
+
+
+	public void testWithAddress()
+		throws Exception {
+
+		String json = "{\n" +
+			"\"sub\": \"248289761001\",\n" +
+			"\"name\": \"Jane Doe\",\n" +
+			"\"email\": \"janedoe@example.com\",\n" +
+			"\"address\": {\n" +
+			"\"formatted\":\"Some formatted\",\n" +
+			"\"street_address\":\"Some street\",\n" +
+			"\"locality\":\"Some locality\",\n" +
+			"\"region\":\"Some region\",\n" +
+			"\"postal_code\":\"1000\",\n" +
+			"\"country\":\"Some country\"\n" +
+			"}   \n" +
+			"}";
+
+		UserInfo userInfo = UserInfo.parse(json);
+
+		assertEquals("248289761001", userInfo.getSubject().getValue());
+		assertEquals("Jane Doe", userInfo.getName());
+		assertEquals("janedoe@example.com", userInfo.getEmail().getAddress());
+
+		Address address = userInfo.getAddress();
+
+		assertEquals("Some formatted", address.getFormatted());
+		assertEquals("Some street", address.getStreetAddress());
+		assertEquals("Some locality", address.getLocality());
+		assertEquals("Some region", address.getRegion());
+		assertEquals("1000", address.getPostalCode());
+		assertEquals("Some country", address.getCountry());
+
+		json = userInfo.toJSONObject().toJSONString();
+
+		userInfo = UserInfo.parse(json);
+
+		assertEquals("248289761001", userInfo.getSubject().getValue());
+		assertEquals("Jane Doe", userInfo.getName());
+		assertEquals("janedoe@example.com", userInfo.getEmail().getAddress());
+
+		address = userInfo.getAddress();
+
+		assertEquals("Some formatted", address.getFormatted());
+		assertEquals("Some street", address.getStreetAddress());
+		assertEquals("Some locality", address.getLocality());
+		assertEquals("Some region", address.getRegion());
+		assertEquals("1000", address.getPostalCode());
+		assertEquals("Some country", address.getCountry());
+	}
 }
