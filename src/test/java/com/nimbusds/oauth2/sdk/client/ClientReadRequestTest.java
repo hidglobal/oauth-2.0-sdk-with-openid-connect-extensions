af8847061064a679fc764b70e3a4d079be1adf85
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2013-12-09 18:49:19 +0200

    Adds ClientReadRequest JUnit test

---
+++ src/test/java/com/nimbusds/oauth2/sdk/client/ClientReadRequestTest.java
@@ -0,0 +1,40 @@
+package com.nimbusds.oauth2.sdk.client;
+
+
+import java.net.URL;
+
+import junit.framework.TestCase;
+
+import com.nimbusds.oauth2.sdk.http.HTTPRequest;
+import com.nimbusds.oauth2.sdk.token.BearerAccessToken;
+
+
+/**
+ * Tests the client read request.
+ */
+public class ClientReadRequestTest extends TestCase {
+
+
+	public void testCycle()
+		throws Exception {
+
+		URL url = new URL("https://c2id.com/client-reg/123");
+		BearerAccessToken accessToken = new BearerAccessToken();
+
+		ClientReadRequest request = new ClientReadRequest(url, accessToken);
+
+		assertEquals(url, request.getURI());
+		assertEquals(accessToken, request.getAccessToken());
+
+		HTTPRequest httpRequest = request.toHTTPRequest();
+
+		assertEquals(HTTPRequest.Method.GET, httpRequest.getMethod());
+		assertEquals(url, httpRequest.getURL());
+		assertEquals(accessToken.toAuthorizationHeader(), httpRequest.getAuthorization());
+
+		request = ClientReadRequest.parse(httpRequest);
+
+		assertEquals(url.toString(), request.getURI().toString());
+		assertEquals(accessToken.getValue(), request.getAccessToken().getValue());
+	}
+}
