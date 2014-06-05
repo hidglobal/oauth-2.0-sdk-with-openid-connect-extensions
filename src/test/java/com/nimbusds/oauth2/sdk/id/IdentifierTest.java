faf5399829d9a647c44fc6e96ad9664c539215b8
Vladimir Dzhuvinov <vladimir@dzhuvinov.com>
2014-06-05 15:42:07 +0300

    Lets Identifier implement java.io.Serializable (issue #90)

--- src/test/java/com/nimbusds/oauth2/sdk/id/IdentifierTest.java
+++ src/test/java/com/nimbusds/oauth2/sdk/id/IdentifierTest.java
@@ -1,18 +1,31 @@
 package com.nimbusds.oauth2.sdk.id;
 
 
-import org.junit.Test;
+import java.io.Serializable;
 
-import static org.junit.Assert.*;
+import junit.framework.TestCase;
+
 
 /**
  * Tests the base Identifier class.
  */
-public class IdentifierTest {
-	
-	@Test
+public class IdentifierTest extends TestCase {
+
+
 	public void testConstant() {
 		
 		assertEquals(32, Identifier.DEFAULT_BYTE_LENGTH);
+	}
+
+
+	public void testForSerializableInstance() {
+
+		assertTrue((new Identifier() {
+
+			public boolean equals(final Object object) {
+				return true;
+			}
+
+		}) instanceof Serializable);
 	}
 }
