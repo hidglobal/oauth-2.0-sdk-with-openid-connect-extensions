69575350e4ebedee742ff22b60c6ed5a4eb00064
Connect2id Support <support@connect2id.com>
2015-03-31 13:53:47 +0300

    Extends HTTP basic auth tests with escaped chars

--- src/test/java/com/nimbusds/oauth2/sdk/auth/ClientSecretBasicTest.java
+++ src/test/java/com/nimbusds/oauth2/sdk/auth/ClientSecretBasicTest.java
@@ -1,10 +1,13 @@
 package com.nimbusds.oauth2.sdk.auth;
 
 
+import java.nio.charset.Charset;
+
 import junit.framework.TestCase;
 
 import com.nimbusds.oauth2.sdk.ParseException;
 import com.nimbusds.oauth2.sdk.id.ClientID;
+import org.apache.commons.codec.binary.Base64;
 
 
 /**
@@ -51,5 +54,49 @@ public class ClientSecretBasicTest extends TestCase {
 
 		assertEquals("s6BhdRkqt3", csb.getClientID().getValue());
 		assertEquals("7Fjfp0ZBr1KtDRbnfVdmIw", csb.getClientSecret().getValue());
+	}
+
+
+	public void testSecretWithBackslashes()
+		throws Exception {
+
+		final String id = "IX-1FXURP1U93W11";
+		final String pw = "cXqXbCJTOUJfypCD92ZNNviQxvYitAN6vH0zF8h/nFy6+yH7ERVlPpIZnUqYfCzaHZYkziI7QBCK88juLTC/t9WwjiMi6WbecE3y+tnD2lniI6PK7n4jMTBhaJPNqfHpvdh13GZswc92HtPSLQYbiKxzgAPhnmFa/1hV+GfmnEp+IXnDRukHA8AaX6L3d4x6T608+2dZRnqOM4+DB7K4vFNm+3bYcEpHz5zhBAulXQMp+GziCoKRcWrQfjHx1cSsmh+R/F6BZLHkVvNF6XKaKA2sDlxc9Bx3EwfNFJYojWiGr+WTD8slrDw6yfbZKTYsfgYFCYf0gSUsV8mHIxaZQA==";
+
+		ClientID clientID = new ClientID(id);
+		Secret secret = new Secret(pw);
+
+		ClientSecretBasic csb = new ClientSecretBasic(clientID, secret);
+
+		String header = csb.toHTTPAuthorizationHeader();
+
+		System.out.println(header);
+		System.out.println("Basic SVgtMUZYVVJQMVU5M1cxMTpjWHFYYkNKVE9VSmZ5cENEOTJaTk52aVF4dllpdEFONnZIMHpGOGhcL25GeTYreUg3RVJWbFBwSVpuVXFZZkN6YUhaWWt6aUk3UUJDSzg4anVMVENcL3Q5V3dqaU1pNldiZWNFM3krdG5EMmxuaUk2UEs3bjRqTVRCaGFKUE5xZkhwdmRoMTNHWnN3YzkySHRQU0xRWWJpS3h6Z0FQaG5tRmFcLzFoVitHZm1uRXArSVhuRFJ1a0hBOEFhWDZMM2Q0eDZUNjA4KzJkWlJucU9NNCtEQjdLNHZGTm0rM2JZY0VwSHo1emhCQXVsWFFNcCtHemlDb0tSY1dyUWZqSHgxY1NzbWgrUlwvRjZCWkxIa1Z2TkY2WEthS0Eyc0RseGM5QngzRXdmTkZKWW9qV2lHcitXVEQ4c2xyRHc2eWZiWktUWXNmZ1lGQ1lmMGdTVXNWOG1ISXhhWlFBPT0=");
+
+		csb = ClientSecretBasic.parse(header);
+
+		assertTrue(clientID.equals(csb.getClientID()));
+		assertTrue(secret.equals(csb.getClientSecret()));
+
+		assertEquals(id, csb.getClientID().getValue());
+		assertEquals(pw, csb.getClientSecret().getValue());
+	}
+
+
+	public void testNonEscapedSecretWithLegacyBasicAuth()
+		throws Exception {
+
+		// Test legacy HTTP basic auth without HTTP URL escape of charc in username + password
+		// See http://tools.ietf.org/html/rfc6749#section-2.3.1
+
+		final String id = "IX-1FXURP1U93W11";
+		final String pw = "cXqXbCJTOUJfypCD92ZNNviQxvYitAN6vH0zF8h\\/nFy6+yH7ERVlPpIZnUqYfCzaHZYkziI7QBCK88juLTC\\/t9WwjiMi6WbecE3y+tnD2lniI6PK7n4jMTBhaJPNqfHpvdh13GZswc92HtPSLQYbiKxzgAPhnmFa\\/1hV+GfmnEp+IXnDRukHA8AaX6L3d4x6T608+2dZRnqOM4+DB7K4vFNm+3bYcEpHz5zhBAulXQMp+GziCoKRcWrQfjHx1cSsmh+R\\/F6BZLHkVvNF6XKaKA2sDlxc9Bx3EwfNFJYojWiGr+WTD8slrDw6yfbZKTYsfgYFCYf0gSUsV8mHIxaZQA==";
+
+		String credentials = id + ":" + pw;
+
+		String header = "Basic " + Base64.encodeBase64String(credentials.getBytes(Charset.forName("UTF-8")));
+
+		assertEquals(header,
+			"Basic SVgtMUZYVVJQMVU5M1cxMTpjWHFYYkNKVE9VSmZ5cENEOTJaTk52aVF4dllpdEFONnZIMHpGOGhcL25GeTYreUg3RVJWbFBwSVpuVXFZZkN6YUhaWWt6aUk3UUJDSzg4anVMVENcL3Q5V3dqaU1pNldiZWNFM3krdG5EMmxuaUk2UEs3bjRqTVRCaGFKUE5xZkhwdmRoMTNHWnN3YzkySHRQU0xRWWJpS3h6Z0FQaG5tRmFcLzFoVitHZm1uRXArSVhuRFJ1a0hBOEFhWDZMM2Q0eDZUNjA4KzJkWlJucU9NNCtEQjdLNHZGTm0rM2JZY0VwSHo1emhCQXVsWFFNcCtHemlDb0tSY1dyUWZqSHgxY1NzbWgrUlwvRjZCWkxIa1Z2TkY2WEthS0Eyc0RseGM5QngzRXdmTkZKWW9qV2lHcitXVEQ4c2xyRHc2eWZiWktUWXNmZ1lGQ1lmMGdTVXNWOG1ISXhhWlFBPT0=");
 	}
 }
