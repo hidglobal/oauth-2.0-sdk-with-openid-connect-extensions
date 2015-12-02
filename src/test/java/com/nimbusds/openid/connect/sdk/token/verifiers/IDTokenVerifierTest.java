97433be2981fca68cc121b1953b0d8a10fcdf61f
Connect2id Support <support@connect2id.com>
2015-12-02 18:58:25 +0200

    Extends tests

--- src/test/java/com/nimbusds/openid/connect/sdk/token/verifiers/IDTokenVerifierTest.java
+++ src/test/java/com/nimbusds/openid/connect/sdk/token/verifiers/IDTokenVerifierTest.java
@@ -1,15 +1,27 @@
 package com.nimbusds.openid.connect.sdk.token.verifiers;
 
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 import java.util.Date;
 
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.JWSHeader;
+import com.nimbusds.jose.crypto.RSASSASigner;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.KeyUse;
+import com.nimbusds.jose.jwk.RSAKey;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.PlainJWT;
+import com.nimbusds.jwt.SignedJWT;
 import com.nimbusds.jwt.proc.BadJWTException;
 import com.nimbusds.oauth2.sdk.id.Audience;
 import com.nimbusds.oauth2.sdk.id.ClientID;
 import com.nimbusds.oauth2.sdk.id.Issuer;
 import com.nimbusds.oauth2.sdk.id.Subject;
+import com.nimbusds.openid.connect.sdk.Nonce;
 import com.nimbusds.openid.connect.sdk.claims.IDTokenClaimsSet;
 import junit.framework.TestCase;
 
@@ -76,5 +88,89 @@ public class IDTokenVerifierTest extends TestCase {
 		} catch (BadJWTException e) {
 			assertEquals("Expired JWT", e.getMessage());
 		}
+	}
+
+
+	public void testVerifySigned()
+		throws Exception {
+
+		KeyPairGenerator gen = KeyPairGenerator.getInstance("RSA");
+		gen.initialize(1024);
+		KeyPair keyPair = gen.generateKeyPair();
+		RSAKey rsaJWK = new RSAKey.Builder((RSAPublicKey)keyPair.getPublic())
+				.privateKey((RSAPrivateKey)keyPair.getPrivate())
+				.keyID("1")
+				.keyUse(KeyUse.SIGNATURE)
+				.build();
+		JWKSet jwkSet = new JWKSet(rsaJWK);
+
+		Issuer iss = new Issuer("https://c2id.com");
+		ClientID clientID = new ClientID("123");
+		Date now = new Date();
+
+		JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+				.issuer(iss.getValue())
+				.subject("alice")
+				.audience(clientID.getValue())
+				.expirationTime(new Date(now.getTime() + 10*60*1000L))
+				.issueTime(now)
+				.build();
+
+		SignedJWT idToken = new SignedJWT(new JWSHeader(JWSAlgorithm.RS256), claimsSet);
+		idToken.sign(new RSASSASigner(rsaJWK));
+
+		IDTokenVerifier idTokenVerifier = new IDTokenVerifier(iss, clientID, JWSAlgorithm.RS256, jwkSet);
+		assertNotNull(idTokenVerifier.getJWSKeySelector());
+		assertNull(idTokenVerifier.getJWEKeySelector());
+
+		IDTokenClaimsSet idTokenClaimsSet = idTokenVerifier.verify(idToken, null);
+		assertEquals(iss, idTokenClaimsSet.getIssuer());
+		assertEquals(new Subject("alice"), idTokenClaimsSet.getSubject());
+		assertTrue(idTokenClaimsSet.getAudience().contains(new Audience("123")));
+		assertNotNull(idTokenClaimsSet.getExpirationTime());
+		assertNotNull(idTokenClaimsSet.getIssueTime());
+	}
+
+
+	public void testVerifySignedWithNonce()
+		throws Exception {
+
+		KeyPairGenerator gen = KeyPairGenerator.getInstance("RSA");
+		gen.initialize(1024);
+		KeyPair keyPair = gen.generateKeyPair();
+		RSAKey rsaJWK = new RSAKey.Builder((RSAPublicKey)keyPair.getPublic())
+				.privateKey((RSAPrivateKey)keyPair.getPrivate())
+				.keyID("1")
+				.keyUse(KeyUse.SIGNATURE)
+				.build();
+		JWKSet jwkSet = new JWKSet(rsaJWK);
+
+		Issuer iss = new Issuer("https://c2id.com");
+		ClientID clientID = new ClientID("123");
+		Date now = new Date();
+
+		JWTClaimsSet claimsSet = new JWTClaimsSet.Builder()
+				.issuer(iss.getValue())
+				.subject("alice")
+				.audience(clientID.getValue())
+				.expirationTime(new Date(now.getTime() + 10*60*1000L))
+				.issueTime(now)
+				.claim("nonce", "xyz")
+				.build();
+
+		SignedJWT idToken = new SignedJWT(new JWSHeader(JWSAlgorithm.RS256), claimsSet);
+		idToken.sign(new RSASSASigner(rsaJWK));
+
+		IDTokenVerifier idTokenVerifier = new IDTokenVerifier(iss, clientID, JWSAlgorithm.RS256, jwkSet);
+		assertNotNull(idTokenVerifier.getJWSKeySelector());
+		assertNull(idTokenVerifier.getJWEKeySelector());
+
+		IDTokenClaimsSet idTokenClaimsSet = idTokenVerifier.verify(idToken, new Nonce("xyz"));
+		assertEquals(iss, idTokenClaimsSet.getIssuer());
+		assertEquals(new Subject("alice"), idTokenClaimsSet.getSubject());
+		assertTrue(idTokenClaimsSet.getAudience().contains(new Audience("123")));
+		assertNotNull(idTokenClaimsSet.getExpirationTime());
+		assertNotNull(idTokenClaimsSet.getIssueTime());
+		assertEquals(new Nonce("xyz"), idTokenClaimsSet.getNonce());
 	}
 }
