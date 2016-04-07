3fa55ea7347a8ac8ab20d27501bb8dd39e8f68c5
Connect2id Support <support@connect2id.com>
2016-04-07 11:50:07 +0300

    Factors out static factory test with HTTP retrieval (iss #173)

--- src/test/java/com/nimbusds/openid/connect/sdk/validators/IDTokenValidatorTest.java
+++ src/test/java/com/nimbusds/openid/connect/sdk/validators/IDTokenValidatorTest.java
@@ -12,13 +12,10 @@ import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
-import static net.jadler.Jadler.*;
-
 import com.nimbusds.jose.*;
 import com.nimbusds.jose.crypto.MACSigner;
 import com.nimbusds.jose.crypto.RSAEncrypter;
 import com.nimbusds.jose.crypto.RSASSASigner;
-import com.nimbusds.jose.jwk.JWK;
 import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.KeyUse;
 import com.nimbusds.jose.jwk.RSAKey;
@@ -51,26 +48,12 @@ import junit.framework.TestCase;
 import org.apache.commons.lang3.ArrayUtils;
 import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apache.commons.lang3.tuple.Pair;
-import org.junit.After;
-import org.junit.Before;
 
 
 /**
  * Tests the ID token verifier.
  */
 public class IDTokenValidatorTest extends TestCase {
-
-
-	@Before
-	public void setUp() {
-		initJadler();
-	}
-
-
-	@After
-	public void tearDown() {
-		closeJadler();
-	}
 
 
 	public void testConstant() {
@@ -539,7 +522,7 @@ public class IDTokenValidatorTest extends TestCase {
 		OIDCProviderMetadata opMetadata = new OIDCProviderMetadata(
 			new Issuer("https://c2id.com"),
 			Collections.singletonList(SubjectType.PUBLIC),
-			URI.create("http://localhost:" + port() + "/jwks.json"));
+			URI.create("https://c2id.com/jwks.json"));
 
 		opMetadata.setIDTokenJWSAlgs(Arrays.asList(JWSAlgorithm.RS256, JWSAlgorithm.HS256));
 		opMetadata.setIDTokenJWEAlgs(Collections.singletonList(JWEAlgorithm.RSA1_5));
@@ -547,119 +530,11 @@ public class IDTokenValidatorTest extends TestCase {
 		opMetadata.setTokenEndpointAuthMethods(Collections.singletonList(ClientAuthenticationMethod.CLIENT_SECRET_BASIC));
 		opMetadata.applyDefaults();
 
-		onRequest()
-			.havingMethodEqualTo("GET")
-			.havingPathEqualTo("/jwks.json")
-			.respond()
-			.withStatus(200)
-			.withHeader("Content-Type", "application/json")
-			.withBody(new JWKSet(Arrays.asList((JWK)rsaJWK1, (JWK)rsaJWK2)).toJSONObject().toJSONString());
-
 		return new ImmutablePair<>(opMetadata, Arrays.asList(rsaJWK1, rsaJWK2));
 	}
 
 
-	public void testStaticFactoryMethod_RS256()
-		throws Exception {
-
-		// Create OP metadata
-		Pair<OIDCProviderMetadata,List<RSAKey>> opInfo = createOPMetadata();
-		OIDCProviderMetadata opMetadata = opInfo.getLeft();
-		RSAKey rsaKey1 = opInfo.getRight().get(0);
-		RSAKey rsaKey2 = opInfo.getRight().get(1);
-
-		// Create client registration
-		OIDCClientMetadata metadata = new OIDCClientMetadata();
-		metadata.setRedirectionURI(URI.create("https://example.com/cb"));
-		metadata.applyDefaults();
-
-		OIDCClientInformation clientInfo = new OIDCClientInformation(new ClientID("123"), new Date(), metadata, new Secret());
-
-		// Create validator
-		IDTokenValidator v = IDTokenValidator.create(opMetadata, clientInfo, null);
-		assertEquals(opMetadata.getIssuer(), v.getExpectedIssuer());
-		assertEquals(clientInfo.getID(), v.getClientID());
-		assertNotNull(v.getJWSKeySelector());
-		assertNull(v.getJWEKeySelector());
-
-		// Check JWS key selector
-		List<Key> matches = v.getJWSKeySelector().selectJWSKeys(new JWSHeader.Builder(JWSAlgorithm.RS256).keyID(rsaKey1.getKeyID()).build(), null);
-		assertEquals(1, matches.size());
-		matches = v.getJWSKeySelector().selectJWSKeys(new JWSHeader.Builder(JWSAlgorithm.RS256).keyID(rsaKey2.getKeyID()).build(), null);
-		assertEquals(1, matches.size());
-		matches = v.getJWSKeySelector().selectJWSKeys(new JWSHeader.Builder(JWSAlgorithm.RS256).build(), null);
-		assertEquals(2, matches.size());
-
-
-		// Create ID token
-		final Date now = new Date();
-		final Date inOneHour = new Date(now.getTime() + 3600*1000L);
-		IDTokenClaimsSet claimsSet = new IDTokenClaimsSet(
-			opMetadata.getIssuer(),
-			new Subject("alice"),
-			new Audience(clientInfo.getID()).toSingleAudienceList(),
-			inOneHour, // exp
-			now); // iat
-
-		SignedJWT idToken = new SignedJWT(new JWSHeader.Builder(JWSAlgorithm.RS256).keyID(rsaKey1.getKeyID()).build(), claimsSet.toJWTClaimsSet());
-		idToken.sign(new RSASSASigner(rsaKey1));
-
-		// Validate
-		IDTokenClaimsSet validated = v.validate(idToken, null);
-		assertEquals(claimsSet.getIssuer(), validated.getIssuer());
-		assertEquals(claimsSet.getSubject(), validated.getSubject());
-		assertEquals(claimsSet.getAudience().get(0), validated.getAudience().get(0));
-		assertEquals(1, validated.getAudience().size());
-		assertEquals(claimsSet.getExpirationTime(), validated.getExpirationTime());
-		assertEquals(claimsSet.getIssueTime(), validated.getIssueTime());
-
-		// Create an ID token with unspecified key ID
-		idToken = new SignedJWT(new JWSHeader.Builder(JWSAlgorithm.RS256).build(), claimsSet.toJWTClaimsSet());
-		idToken.sign(new RSASSASigner(rsaKey1));
-
-		// Validate again
-		validated = v.validate(idToken, null);
-		assertEquals(claimsSet.getIssuer(), validated.getIssuer());
-		assertEquals(claimsSet.getSubject(), validated.getSubject());
-		assertEquals(claimsSet.getAudience().get(0), validated.getAudience().get(0));
-		assertEquals(1, validated.getAudience().size());
-		assertEquals(claimsSet.getExpirationTime(), validated.getExpirationTime());
-		assertEquals(claimsSet.getIssueTime(), validated.getIssueTime());
-
-		// Sign ID token with invalid RSA key
-		KeyPairGenerator pairGen = KeyPairGenerator.getInstance("RSA");
-		pairGen.initialize(1024);
-		KeyPair keyPair = pairGen.generateKeyPair();
-
-		final RSAKey badKey = new RSAKey.Builder((RSAPublicKey) keyPair.getPublic())
-			.privateKey((RSAPrivateKey) keyPair.getPrivate())
-			.keyID("1")
-			.build();
-
-		idToken = new SignedJWT(new JWSHeader.Builder(JWSAlgorithm.RS256).keyID(rsaKey1.getKeyID()).build(), claimsSet.toJWTClaimsSet());
-		idToken.sign(new RSASSASigner(badKey));
-
-		try {
-			v.validate(idToken, null);
-			fail();
-		} catch (BadJWSException e) {
-			assertEquals("Signed JWT rejected: Invalid signature", e.getMessage());
-		}
-
-		// Sign ID token with RSA key with unexpected key ID
-		idToken = new SignedJWT(new JWSHeader.Builder(JWSAlgorithm.RS256).keyID("XXXXXXX").build(), claimsSet.toJWTClaimsSet());
-		idToken.sign(new RSASSASigner(rsaKey1));
-
-		try {
-			v.validate(idToken, null);
-			fail();
-		} catch (BadJOSEException e) {
-			assertEquals("Signed JWT rejected: No matching key(s) found", e.getMessage());
-		}
-	}
-
-
-	public void _testStaticFactoryMethod_HS256()
+	public void testStaticFactoryMethod_HS256()
 		throws Exception {
 
 		// Create OP metadata
@@ -687,6 +562,9 @@ public class IDTokenValidatorTest extends TestCase {
 		assertEquals(1, matches.size());
 		assertTrue(ArrayUtils.isEquals(clientInfo.getSecret().getValueBytes(), matches.get(0).getEncoded()));
 
+		matches = v.getJWSKeySelector().selectJWSKeys(new JWSHeader.Builder(JWSAlgorithm.HS256).keyID("xxx").build(), null);
+		assertTrue(matches.isEmpty());
+
 
 		// Create ID token
 		final Date now = new Date();
@@ -700,6 +578,7 @@ public class IDTokenValidatorTest extends TestCase {
 
 		SignedJWT idToken = new SignedJWT(new JWSHeader(JWSAlgorithm.HS256), claimsSet.toJWTClaimsSet());
 		idToken.sign(new MACSigner(clientInfo.getSecret().getValueBytes()));
+		idToken = SignedJWT.parse(idToken.serialize());
 
 		// Validate
 		IDTokenClaimsSet validated = v.validate(idToken, null);
